31550ca5888684af859d13ce55d0c22b53e6fa41
Katarzyna Dykiert <kasia.dykiert@gmail.com>
2018-11-12 14:55:03 +0100

    initialize project

---
+++ Fonoteka/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Fonoteka
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Fonoteka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fonoteka.MvcApplication" Language="C#" %>
